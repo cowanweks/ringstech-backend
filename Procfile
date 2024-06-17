5533a4f296f9218c79ac2f795af9177b346e87ba
cowanweks <cowannwekesa@gmial.com>
2024-06-17 16:38:05 +0300

    updated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 run.py
+web: gunicorn --workers=2 run:flask_app
--- run.py
+++ run.py
@@ -1,24 +1,27 @@
 from app import create_app
-from waitress import serve
+# from waitress import serve
 
 
-def main():
-    """Create the flask application"""
-    flask_app = create_app()
+# def main():
+#     """Create the flask application"""
+#     flask_app = create_app()
 
-    if flask_app.config.get("ENV") == "production":
-        try:
-            serve(
-                app=flask_app,
-                host="0.0.0.0",
-                port=flask_app.config.get("PORT"),
-                url_prefix="/bytabler",
-            )
-        except Exception as ex:
-            print(str(ex))
+#     if flask_app.config.get("ENV") == "production":
+#         try:
+#             serve(
+#                 app=flask_app,
+#                 host="0.0.0.0",
+#                 port=flask_app.config.get("PORT"),
+#                 url_prefix="/bytabler",
+#             )
+#         except Exception as ex:
+#             print(str(ex))
 
-    flask_app.run(host=flask_app.config.get("HOST"), port=flask_app.config.get("PORT"))
+#     flask_app.run(host=flask_app.config.get("HOST"), port=flask_app.config.get("PORT"))
+
+flask_app = create_app()
+flask_app.run(host=flask_app.config.get("HOST"), port=flask_app.config.get("PORT"))
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
